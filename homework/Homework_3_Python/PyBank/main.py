8f903d853e703a9c43269d780a6207f5d0bc6842
jfandata <jfan07@gmail.com>
2019-07-18 00:02:56 -0500

    updated python homework

---
+++ homework/Homework_3_Python/PyBank/main.py
@@ -0,0 +1,72 @@
+import os
+import csv
+
+csvpath = os.path.join("Resources/budget_data.csv")
+
+with open (csvpath, newline="") as csvfile:
+    csv_reader =csv.reader(csvfile, delimiter=",")
+    csv_headers = next(csv_reader, None)
+
+#with open (csvpath, 'r') as csv_reader:
+#    lines = csv_reader.read()
+
+
+# count the months
+# 1. create lists to store months and profitLoss
+    months = []
+    profit = []
+
+    for row in csv_reader:
+        months.append(row[1])
+        profit.append(int(row[1]))
+
+    total_months = len(months)
+    print (f"Total Months: {total_months}")
+
+# add the profit/Loss to get revenues
+    revenues = 0
+    i = 1
+    for i in range(total_months):
+        revenues = revenues + int(profit[i])
+    print (f"Total: ${revenues}")
+
+# find the average change per month
+# 1. find change in profit/loss per month
+    change = []
+    j = 0
+    k = 0
+
+    for j in range (1, total_months):
+        if j == 0:
+            change.append(0)
+        else:
+            change.append(int(profit[j])-int(profit[k]))
+            k += 1
+    #print (change)
+# 2. sum the monthly changes and divide by total_months
+    average_month = ((sum(change))/(len(change)))
+    print (f"Average Change: ${round((average_month),2)}")
+
+# greatest increase in profits
+    max_change = max(change)
+    print (f"${max_change}")
+# greatest decrease in profits
+    min_change = min(change)
+    print (f"${min_change}")
+    
+# greatest increase in profits
+    max_change = max(change)
+    print (f"Greatest Increase in Revenues: ${max_change}")
+# greatest decrease in profits
+    min_change = min(change)
+    print (f"Greasest Decrease in Revenues:  ${min_change}")
+
+# export results
+exportpath = ("Results.txt")
+with open(exportpath, "w") as textfile:
+        textfile.write(f"Total Months: {total_months}")
+        textfile.write(f"Total: ${revenues}")
+        textfile.write(f"Average Change: ${round((average_month),2)}")
+        textfile.write(f"Greatest Increase in Revenues: ${max_change}")
+        textfile.write(f"Greasest Decrease in Revenues:  ${min_change}")
+        
---
+++ homework/Homework_3_Python/PyPoll/main.py
@@ -0,0 +1,73 @@
+import os
+import csv
+
+csvpath = os.path.join("Resources/election_data.csv")
+
+with open(csvpath, newline="") as csvfile:
+    csv_reader =csv.reader(csvfile, delimiter=",")
+    csv_headers = next(csv_reader, None)
+    #print (f"CSV Header: {csv_headers}"
+
+    # print heading 
+    print ("Election Results")
+    print ("-----------------------")
+
+
+    # Variables
+    candidate_list = []
+    count_votes = 0
+    
+    # Loop through csv_reader
+    for row in csv_reader:
+        # if value in Candidate column is not in candidate_list,
+        # append to candidate_list
+        if row[2] not in candidate_list:
+            count_votes += 1
+            candidate_list.append(row[2])
+        else: # if not in list, just add the vote count
+            count_votes += 1
+
+        # print (len(candidate_list)) = 4
+    
+    # count votes per candidate
+    i = 0 
+    num_candidates = len(candidate_list)
+    candidate_votes = [0]*num_candidates
+
+    for i in range(num_candidates):
+        if row[2] == candidate_list[i]:
+            candidate_votes[i] += 1
+
+    most_vote = max(candidate_votes)
+    vote_index = candidate_votes.index(most_vote)
+    winner = candidate_list[vote_index]
+
+    #print (winner)
+
+    # percent per candidate
+    j = 0
+    percent_of_votes = []
+
+    for j in range(num_candidates):
+        percent = round(candidate_votes[j]/count_votes * 100,2)
+        percent_of_votes.append(percent)
+
+
+    print ("Election Results")
+    print ("-----------------------------")
+    print (f"Total Votes: {count_votes}")
+    print ("-----------------------------")
+    print (f"Name: {candidate_list} {percent_of_votes}, {candidate_votes}")
+    print ("-----------------------------")
+    print (f"Winner: {winner}")
+
+exportpath = ("Results.txt")
+with open(exportpath, "w") as textfile:
+        textfile.write("Election Results")
+        textfile.write("-----------------------------")
+        textfile.write(f"Total Votes: {count_votes}")
+        textfile.write("-----------------------------")
+        textfile.write(f"Name: {candidate_list} {percent_of_votes}, {candidate_votes}")
+        textfile.write("-----------------------------")
+        textfile.write(f"Winner: {winner}")
+    
